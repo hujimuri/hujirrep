827301ae5af09ef32031efc123cdb1f117ae4ef8
Training_b6b.01.07 <Training_b6b.01.07@accenture.com>
2019-09-04 15:25:47 +0530

    my file

---
+++ bdc_demo_01_def.java
@@ -0,0 +1,48 @@
+package bdcstepdefinations;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class bdc_demo_01_def {
+	@Given("user launched the webshop in chrome browser")
+	public void user_launched_the_webshop_in_chrome_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.println("blah blah");
+	}
+
+	@Then("user search for the items of his choice")
+	public void user_search_for_the_items_of_his_choice() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new cucumber.api.PendingException();
+		System.out.println("blah blah");
+	}
+
+	@Then("add that item to the cart")
+	public void add_that_item_to_the_cart() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new cucumber.api.PendingException();
+		System.out.println("blah blah");
+	}
+
+	@Given("user has already placed an order for his fav item")
+	public void user_has_already_placed_an_order_for_his_fav_item() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.println("blah blah");
+	}
+
+	@Given("user tries to access theorder tracking dashboard")
+	public void user_tries_to_access_theorder_tracking_dashboard() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.println("blah blah");
+	}
+
+	@Then("identifies the states of an item")
+	public void identifies_the_states_of_an_item() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.println("blah blah");
+	}
+}
---
+++ bdc_demo_02_def.java
@@ -0,0 +1,41 @@
+package bdcstepdefinations;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class bdc_demo_02_def {
+	@Given("user access signup page")
+	public void user_access_signup_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new cucumber.api.PendingException();
+		System.out.print("Hello u are mad");
+	}
+
+	@Given("get the credentials")
+	public void get_the_credentials() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.print("Hello u are mad");
+	}
+
+	@Given("user has valid credentials")
+	public void user_has_valid_credentials() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.print("Hello u are mad");
+	}
+
+	@Given("provide valid cred for login")
+	public void provide_valid_cred_for_login() {
+	    // Write code here that turns the phrase above into concrete actions
+	    //throw new cucumber.api.PendingException();
+		System.out.print("Hello u are mad");
+	}
+
+	@Then("verifies the login status")
+	public void verifies_the_login_status() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new cucumber.api.PendingException();
+		System.out.print("Hello u are mad");
+	}
+}
